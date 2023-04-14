a85998d91dd6e8533cb54a7f794a915f70cf49fd
Susmita-Dey <susmitadey475@gmail.com>
2023-04-14 12:31:08 +0530

    adding Features Section

---
+++ code-editor/components/Features.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Features() {
+    return (
+        <section className="container-mx-auto mb-4">
+            <h1>Hello World</h1>
+        </section>
+    )
+}
