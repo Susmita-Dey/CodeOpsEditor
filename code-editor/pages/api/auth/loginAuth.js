1fbd6d40e8d49e9e3d7b74d5b3734056e4493b15
TuhinBanerjee31 <tuhinbanerjee0231@gmail.com>
2023-04-14 22:29:58 +0530

    Added login/signup with DB

---
+++ code-editor/pages/api/auth/loginAuth.js
@@ -0,0 +1,16 @@
+import connect from "../../../lib/mongodb";
+import User from "../../../models/user"
+
+connect()
+
+export default async function handler(req,res){
+
+    const {email,password}=req.body
+    const user = await User.findOne({email,password})
+    if(!user){
+        return res.json({status:'Not able to find the user'})
+    }
+    else{
+        res.redirect('/writecode')
+    }
+}
--- code-editor/pages/signup.js
+++ code-editor/pages/signup.js
@@ -1,6 +1,6 @@
-import SignUpComponent from '@/components/SignUpComponent'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
+import Link from "next/link"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +11,30 @@ export default function Login() {
                 <Head>
                     <title>CodeOpsEditor | Sign Up</title>
                 </Head>
-                <SignUpComponent />
+
+                <section className="container mx-auto flex flex-col justify-center items-center">
+                <div className="flex flex-col text-xl gap-4">
+                    <form action="/api/auth/signupAuth" method="POST" className="flex flex-col gap-4">
+                        <div className="flex flex-col gap-2">
+                            <label>Enter Name:</label>
+                            <input type="text" name="fname" id="fname" placeholder="Susmita" />
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <label>Enter Email:</label>
+                            <input type="email" name="email" id="email" placeholder="susmita@example.com" />
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <label>Enter Password:</label>
+                            <input type="password" name="password" id="password" placeholder="*123165$5%^*SDYTRfgds" />
+                        </div>
+                        <div className="px-4 py-2 bg-blue-500 text-white text-center">
+                        <input type="submit" value="Register"></input>
+                        </div>
+                        <p className="my-2 text-lg font-medium">Already have an account? <Link href='/login' className="text-blue-500 underline underline-offset-2">Login</Link> now.</p>
+                    </form>
+                </div>
+            </section>
+
             </div>
         </>
     )
