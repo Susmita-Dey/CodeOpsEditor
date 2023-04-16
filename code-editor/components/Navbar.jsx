bdc4a7285c9ec3995e8de180c38744e80f54d3ca
Susmita-Dey <susmitadey475@gmail.com>
2023-04-16 10:57:01 +0530

    remove contact form

--- code-editor/components/Navbar.jsx
+++ code-editor/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { GoThreeBars } from 'react-icons/go'
 import { ImCross } from 'react-icons/im'
 import Link from "next/link";
 import { AiFillCloud } from 'react-icons/ai'
+import { BsGithub } from "react-icons/bs";
 
 export default function Navbar() {
     const [mobile, setMobile] = useState(true)
@@ -11,9 +12,9 @@ export default function Navbar() {
             <nav className="container mx-auto">
                 <div
                     id="navbar"
-                    className="flex flex-wrap flex-row items-center justify-around px-5 sticky top-0 my-3"
+                    className="flex flex-wrap flex-row items-center justify-around px-5 sticky top-0 my-3 md:gap-0 gap-2"
                 >
-                    <button onClick={() => setMobile(!mobile)}>
+                    {/* <button onClick={() => setMobile(!mobile)}>
                         {!mobile ? (
                             <ImCross
                                 id="hamicon"
@@ -25,21 +26,21 @@ export default function Navbar() {
                                 className=" fill-black ml-5 text-3xl border-t-0 border-b-0  md:hidden"
                             />
                         )}
-                    </button>
+                    </button> */}
                     <Link
-                        href="/" className="flex title-font font-semibold text-3xl text-gray-900 md:-ml-96"
+                        href="/" className="flex flex-row gap-1 title-font font-semibold text-3xl text-gray-900 md:-ml-96"
                     >
-                        <AiFillCloud /> CodeOpsEditor
+                        <AiFillCloud className="mt-1 text-4xl" /> CodeOpsEditor
                     </Link>
-                    <ul className={mobile ? "navlinks" : "navlinksmobile"}
-                        onClick={() => setMobile(true)} >
-                        <ul className="flex flex-row justify-evenly gap-4 md:gap-8 items-center text-lg">
-                            <Link href="/">Home</Link>
-                            <Link href="#features">Features</Link>
-                            <Link href="#team">Team</Link>
-                            <Link href="/contact">Contact</Link>
-                        </ul>
+                    {/* <ul className={mobile ? "navlinks" : "navlinksmobile"}
+                        onClick={() => setMobile(true)} > */}
+                    <ul className="flex flex-row justify-evenly gap-4 md:gap-8 items-center text-lg">
+                        <Link href="/">Home</Link>
+                        <Link href="#features">Features</Link>
+                        <Link href="#team">Team</Link>
+                        <Link href="https://github.com/Susmita-Dey/CodeOpsEditor/" target='_blank' className="flex flex-row gap-1">Contribute <BsGithub className="text-xl mt-1" /></Link>
                     </ul>
+                    {/* </ul> */}
 
                     <Link href="/signup">
                         <button className="px-5 py-2 text-white rounded-md bg-gradient-to-r from-blue-500 to-purple-500 text-lg">
